2285e78e32502f99336ad3e5558dd3033474d0db
Kevin Omar Marin Gonzalez <100540674+OmarMG05@users.noreply.github.com>
2024-03-18 13:48:30 -0600

    Add files via upload

---
+++ 1P/act4_HyCAbstrc/Main.kt
@@ -0,0 +1,21 @@
+import Practicas_Clases.Clss_International
+import Practicas_Clases.Clss_Nacional
+import Practicas_Clases.Clss_TempBaja
+
+fun main() {
+    // Prueba de clase Clss_Nacional
+    val viajeNacional = Clss_Nacional("Monterrey")
+    viajeNacional.cotizarPrecio(5)
+
+    // Prueba de clase Clss_TempBaja
+    val viajeTempBaja = Clss_TempBaja("San Miguel de Allende")
+    viajeTempBaja.cotizarPrecio(7)
+    viajeTempBaja.reservar(7)
+    println(viajeTempBaja.cancelar())
+
+    // Prueba de clase Clss_International
+    val viajeInternacional = Clss_International("Alemania", "Munich")
+    viajeInternacional.cotizarPrecio(10)
+
+    // Puedes agregar más pruebas para otras funcionalidades
+}
